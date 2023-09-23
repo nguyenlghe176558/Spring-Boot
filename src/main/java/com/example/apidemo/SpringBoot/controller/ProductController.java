cf26966d73eb5b2157e87cf4ed064397b72ff384
Le Gia Nguyen <nguyenlghe176558@fpt.edu.vn>
2023-09-23 15:10:32 +0700

    s

--- src/main/java/com/example/apidemo/SpringBoot/controller/ProductController.java
+++ src/main/java/com/example/apidemo/SpringBoot/controller/ProductController.java
@@ -2,17 +2,31 @@ package com.example.apidemo.SpringBoot.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.apidemo.SpringBoot.model.Product;
+import com.example.apidemo.SpringBoot.repository.ProductRepository;
+
 @RestController
 @RequestMapping(path = "api/v1/Products")
 public class ProductController {
+    // Dependency injection
+    @Autowired
+    private ProductRepository repository;
+
     @GetMapping("")
     /// localhost:8080/api/v1/Products
-    List<String> getAllProducts(){
-        return List.of("iPhone", "iPad");
-    }    
-    
+    List<Product> getAllProducts(){
+        return repository.findAll();
+    }
+
+
+    // List<Product> getAllProducts() {
+    //     return List.of(new Product(1L, "iPhone 15", 2023, 999.9, "apple.com"),
+    //             new Product(2L, "MacBook Air", 2022, 1099.9, "apple.com"));
+    // }
+
 }
---
+++ src/main/java/com/example/apidemo/SpringBoot/database/Database.java
@@ -0,0 +1,29 @@
+package com.example.apidemo.SpringBoot.database;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.example.apidemo.SpringBoot.model.Product;
+import com.example.apidemo.SpringBoot.repository.ProductRepository;
+
+@Configuration
+public class Database {
+    // logger
+    private static final Logger logger = LoggerFactory.getLogger(Database.class);
+    @Bean
+    CommandLineRunner initDatabase(ProductRepository productRepository) {
+        return new CommandLineRunner() {
+            @Override
+            public void run(String... args) throws Exception {
+                Product productA = new Product("iPhone 15", 2023, 999.9, "apple.com");
+                Product productB = new Product("MacBook Air", 2022, 1099.9, "apple.com");
+                logger.info("insert database " + productRepository.save(productA));
+                logger.info("insert database " + productRepository.save(productB));
+            }
+        };
+    }
+
+}
